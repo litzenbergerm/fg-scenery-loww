02db67ad458d2ae5b6eecff5c9237e5fa0f5a48d
litzenbergerm <67824090+litzenbergerm@users.noreply.github.com>
2021-10-17 18:16:22 +0200

    added texture 'atlas' generation script

---
+++ tools/gen-atlas.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export PNG_PATH=../data/textures/png_512
+export ATLAS_DIR=/home/martin/_fghome_/Export
+
+#cd $ATLAS_DIR
+cd $PNG_PATH
+
+for a in $ATLAS_DIR/*.atlas
+do
+    arg=
+    
+    while IFS= read -r f
+    do
+      arg="$arg $f"
+    done < "$a"
+    
+    convert -append $arg "${a%.*}".png
+    
+done
+
+
+echo "====================="
+echo "done."
+echo "====================="
